6a3790107f7cc793230399e2a60c048ab136a8fd
NicolasLopezSTC <gclan149@gmail.com>
2023-10-12 02:58:32 -0500

    Components done
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,8 @@
 <script setup>
+import MainLayout from "@/components/MainLayout.vue"
 </script>
 
 <template>
-  <main>
-    <div class="card">
-    <RouterView />
-  </div>
- </main>
- <p class="score">Score: 100</p>
+<MainLayout />
 </template>
 
-<style lang="postcss" scoped>
-main {
-  @apply flex justify-center items-center h-screen bg-gradient-to-t from-green-200 via-green-300 to-blue-500
-}
-   & .card {
-    @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
-  
-}
-.score {
-  @apply absolute bottom-0 right-0 m-8 text-2xl font-bold text-slate-700
-}
-
-</style>
