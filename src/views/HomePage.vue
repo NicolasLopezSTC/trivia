9c00e38e6f1354946498619989f38340a2f31b14
NicolasLopezSTC <gclan149@gmail.com>
2023-10-12 03:06:59 -0500

    making progress
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,60 +1,42 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue'
 import useAPI from '@/composables/useAPI'
+import BaseTitle from '@/components/BaseTitle.vue'
 
 const api = useAPI()
 const categories = ref([])
+
 onMounted(async () => {
   categories.value = await api.getCategories()
 })
-
 </script>
 
-
 <template>
-<div class="brand">
-  <img class="logo" src="logo.svg" alt="logo" />
-  <h1 class="title">Triviantastic</h1>
-  <img class="logo" src="logo.svg" alt="logo" />
-</div>
-<div class="categories">
-  <RouterLink    
-     v-for="category in categories"
-     :key="category.id"
-     :to="`/question/category/${category.id}`"
-     class="category"
-  >
-     {{ category.name }} 
-
-  </RouterLink>
+  <!--  -->
+  <BaseTitle>
+    <template #logo>
+      <img src="logo.svg" alt="logo" />
+    </template>
+    Triviantastic
+  </BaseTitle>
+  <div class="categories">
+    <RouterLink v-for="category in categories" :key="category.id" :to="`/question/category/${category.id}`"
+      class="category">
+      {{ category.name }}
+    </RouterLink>
   </div>
-  
 </template>
 
 <style lang="postcss" scoped>
-
-.brand {
-@apply flex items-center justify-center gap-4;
-
-
- & .logo {
-  @apply h-16 w-16;
- }
- & .title {
-  @apply text-6xl font-thin tracking-widest text-slate-700 uppercase;
- }
-}
-
 .categories {
-@apply grid flex-grow grid-cols-4 gap-12;
+  @apply grid flex-grow grid-cols-4 gap-12;
 
-& .category {
-@apply text-center flex h-32  items-center justify-center rounded-lg border-purple-500 py-4 font-bold uppercase text-slate-600 transition-colors duration-300;
+  & .category {
+    @apply flex h-32 items-center justify-center rounded-lg border-4 border-green-500 py-4 text-center font-bold uppercase text-slate-600 transition-colors duration-300;
 
-&:hover {
-  @apply cursor-pointer border-red-500 bg-red-500 text-white;
+    &:hover {
+      @apply cursor-pointer border-red-500 bg-red-500 text-white;
     }
   }
 }
-
-</style>@/composables/useApi
+</style>
