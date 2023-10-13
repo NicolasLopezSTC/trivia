687a6e29fd8b2fd08ed60d6ed9a3bab78946c201
nhinojosa <nhinojosa@southtexascollege.edu>
2023-10-13 06:45:04 -0500

    fixed Difficulty.chip and router.js

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -28,7 +28,7 @@ const handleAnswer = (points) => {
   showNotification.value = true
   setTimeout(() => {
     changeScore(points)
-    router.push('/triviantastic/')
+    router.push('/trivia/')
   }, 1000)
 
 }
