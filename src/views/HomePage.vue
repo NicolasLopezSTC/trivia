f06330981d2277751679955338fc691a37762d77
NicolasLopezSTC <gclan149@gmail.com>
2023-10-13 03:19:35 -0500

    complete
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,42 +1,56 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref } from 'vue';
 import useAPI from '@/composables/useAPI'
 import BaseTitle from '@/components/BaseTitle.vue'
-
-const api = useAPI()
-const categories = ref([])
+const {categories, getCategories} = useAPI()
 
 onMounted(async () => {
-  categories.value = await api.getCategories()
+  
+    await getCategories()
+
 })
 </script>
 
 <template>
-  <!--  -->
   <BaseTitle>
     <template #logo>
-      <img src="logo.svg" alt="logo" />
+      <img src="/logo.svg" alt="" />
     </template>
     Triviantastic
+    
   </BaseTitle>
-  <div class="categories">
-    <RouterLink v-for="category in categories" :key="category.id" :to="`/question/category/${category.id}`"
-      class="category">
+  <div v-if="categories.length > 0" class="categories">
+    <RouterLink :to="`/question/category/${category.id}`" v-for="category in categories" :key="category.id" class="category">
       {{ category.name }}
     </RouterLink>
   </div>
 </template>
 
-<style lang="postcss" scoped>
+<style lang ="postcss" scoped>
+.brand {
+  @apply flex items-center justify-center gap-4;
+
+  & .logo {
+    @apply h-16 w-16;
+  }
+
+  & .title {
+    @apply text-6xl font-thin tracking-widest text-slate-700 uppercase;
+  }
+}
+
 .categories {
   @apply grid flex-grow grid-cols-4 gap-12;
 
   & .category {
-    @apply flex h-32 items-center justify-center rounded-lg border-4 border-green-500 py-4 text-center font-bold uppercase text-slate-600 transition-colors duration-300;
+    @apply text-center flex h-32 items-center justify-center rounded-lg border-4 border-green-500 py-4 font-bold uppercase transition-colors duration-300;
 
-    &:hover {
-      @apply cursor-pointer border-red-500 bg-red-500 text-white;
-    }
+      &:hover {
+        @apply cursor-pointer border-red-500 bg-red-500 text-white;
+      }
   }
+
+
 }
+
 </style>
